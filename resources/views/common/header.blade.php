99182e405adfa7b5cc6ae18d0e8bdb9ef31137a4
tsukada <soranohoshi739@gmail.com>
2022-09-13 00:24:28 +0900

    mypage,Toppage周り

--- resources/views/common/header.blade.php
+++ resources/views/common/header.blade.php
@@ -12,7 +12,7 @@
         <div class="container-fluid">
           <a class="navbar-brand">logo</a>
           <form class="d-flex">
-            <a><button class="btn btn-outline-success" type="submit">LOGIN</button></a>
+            <a href="/mypage"><button class="btn btn-outline-success" type="button">Login</button></a>
             <button class="btn btn-outline-success" type="submit">カート画像</button>
           </form>
         </div>
