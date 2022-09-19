6105ab2e1a2d429e1a1d2ff0863049cfc800518f
tsukada <soranohoshi739@gmail.com>
2022-09-19 23:41:44 +0900

    バリデーション

--- app/Http/Requests/EntryRequest.php
+++ app/Http/Requests/EntryRequest.php
@@ -26,11 +26,42 @@ class EntryRequest extends FormRequest
         return [
             'name' => ["required"],
             'email' => ['required'],
-            'postal_code' => ['required'],
+            'postal_code' => ['required', 'numeric'],
             'pref_id' => ['required'],
             'city' => ['required'],
             'addres' => ['required'],
-            'password' => ['required'],
+            'password' => ['required', 'min:8'],
         ];
+    }
+
+    /**
+     * データ整形
+     *
+     * @param Request $request
+     */
+    protected function prepareForValidation()
+    {
+        // 名前
+        $sanitaizeName = str_replace(" ", "", $this->input('name'));
+        $sanitaizeName = mb_convert_kana($sanitaizeName, 'K');
+
+        // メールアドレス
+        $sanitaizeMail = str_replace(" ", "", $this->input('email'));
+        $sanitaizeMail = mb_convert_kana($sanitaizeMail, 'r');
+        $sanitaizeMail = mb_convert_kana($sanitaizeMail, 'n');
+
+        // 郵便番号
+        $sanitaizePostal = str_replace(" ", "", $this->input('postal_code'));
+        $sanitaizePostal = mb_convert_kana($sanitaizePostal, 'n');
+
+        // パスワード
+        $sanitaizePassword = mb_convert_kana($this->input('password'), 'n');
+
+        $this->merge([
+            'name' => $sanitaizeName,
+            'email' => $sanitaizeMail,
+            'postal_code' => $sanitaizePostal,
+            'password' => $sanitaizePassword,
+        ]);
     }
 }
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -29,24 +29,25 @@
                             <div class="col-md-9">
                                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror"
                                     name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                                @error('email')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                                @enderror
                             </div>
-                            @error('email')
-                            <span class="invalid-feedback" role="alert">
-                                <strong>{{ $message }}</strong>
-                            </span>
-                            @enderror
+
                         </div>
                         <div class="mb-3 row form-input">
                             <label for="name" class="col-md-3 col-form-label text-md-start">名前</label>
                             <div class="col-md-9">
                                 <input id="name" type="text" class="form-control @error('name') is-invalid @enderror"
                                     name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+                                @error('name')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                                @enderror
                             </div>
-                            @error('name')
-                            <span class="invalid-feedback" role="alert">
-                                <strong>{{ $message }}</strong>
-                            </span>
-                            @enderror
                         </div>
                         <div class="mb-3 row form-input">
                             <label for="postal_code" class="col-md-3 col-form-label text-md-start">郵便番号</label>
@@ -54,40 +55,43 @@
                                 <input id="postal_code" type="text"
                                     class="form-control @error('postal_code') is-invalid @enderror" name="postal_code"
                                     value="{{ old('postal_code') }}" required autocomplete="postal_code" autofocus>
+                                @error('postal_code')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                                @enderror
                             </div>
-                            @error('postal_code')
-                            <span class="invalid-feedback" role="alert">
-                                <strong>{{ $message }}</strong>
-                            </span>
-                            @enderror
+
                         </div>
                         <div class="mb-3 row form-input">
                             <label for="pref_id" class="col-md-3 col-form-label text-md-start">都道府県</label>
                             <div class="col-md-9">
-                                <select class="form-select" aria-label="Default select example" name="pref_id">
+                                <select class="form-select @error('pref_id') is-invalid @enderror"
+                                    aria-label="Default select example" name="pref_id" required>
                                     <option value="">選択してください</option>
                                     <option value="1">One</option>
                                     <option value="2">Two</option>
                                     <option value="3">Three</option>
                                 </select>
+                                @error('pref_id')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                                @enderror
                             </div>
-                            @error('pref_id')
-                            <span class="invalid-feedback" role="alert">
-                                <strong>{{ $message }}</strong>
-                            </span>
-                            @enderror
+
                         </div>
                         <div class="mb-3 row form-input">
                             <label for="city" class="col-md-3 col-form-label text-md-start">市区町村</label>
                             <div class="col-md-9">
                                 <input id="city" type="text" class="form-control @error('city') is-invalid @enderror"
                                     name="city" value="{{ old('city') }}" required autocomplete="city" autofocus>
+                                @error('city')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                                @enderror
                             </div>
-                            @error('city')
-                            <span class="invalid-feedback" role="alert">
-                                <strong>{{ $message }}</strong>
-                            </span>
-                            @enderror
                         </div>
                         <div class="mb-3 row form-input">
                             <label for="addres" class="col-md-3 col-form-label text-md-start">番地以外</label>
@@ -95,12 +99,13 @@
                                 <input id="addres" type="text"
                                     class="form-control col-md-2 @error('addres') is-invalid @enderror" name="addres"
                                     value="{{ old('addres') }}" required autocomplete="addres" autofocus>
+                                @error('addres')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                                @enderror
                             </div>
-                            @error('addres')
-                            <span class="invalid-feedback" role="alert">
-                                <strong>{{ $message }}</strong>
-                            </span>
-                            @enderror
+
                         </div>
                         <div class="mb-3 row form-input">
                             <label for="email" class="col-md-3 col-form-label text-md-start">パスワード</label>
@@ -108,12 +113,13 @@
                                 <input id="password" type="password"
                                     class="form-control  @error('password') is-invalid @enderror" name="password"
                                     required autocomplete="current-password">
+                                @error('password')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                                @enderror
                             </div>
-                            @error('password')
-                            <span class="invalid-feedback" role="alert">
-                                <strong>{{ $message }}</strong>
-                            </span>
-                            @enderror
+
                         </div>
                         <div class="row mb-3 mt-5">
                             <div>
